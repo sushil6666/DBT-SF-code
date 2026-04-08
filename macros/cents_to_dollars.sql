f05ef4842e2e81cfb2189672a70914d837a94abe
Sushil <sushil@example.com>
2026-04-08 09:36:47 +0000

    Update project: add first_dbt_model, remove unused files

--- macros/cents_to_dollars.sql
+++
@@ -1,3 +0,0 @@
-{% macro cents_to_dollars(column_name) -%}
-    ({{ column_name }} / 100)::numeric(16, 2)
-{%- endmacro %}
--- tests/is_odd.sql
+++
@@ -1,7 +0,0 @@
-{% test is_odd(model, column_name) %}
-
-    select {{ column_name }}
-    from {{ model }}
-    where {{ column_name }} % 2 = 0
-
-{% endtest %}
